7f52e23c199380235cffbab5f1705682aebf04ca
Thais Campos <thaismca.bio@gmail.com>
2023-02-18 12:04:52 -0800

    Refactored code to reduce function calls

--- Udemy - 100 days of Python/Begginer sections/day-11_blackjack/main.py
+++ Udemy - 100 days of Python/Begginer sections/day-11_blackjack/main.py
@@ -16,35 +16,52 @@ def start_game():
     # draw cards to the dealer and add them 
     # continue to draw while the sum of the hand is less than 17
     hand_dealer = []
-    while sum_hand(hand_dealer) < 17:
+    score_dealer = 0
+    while score_dealer < 17:
         draw_cards(hand_dealer)
+        score_dealer = sum_hand(hand_dealer)
+    # get a reference to the list of cards in dealer's hand
+    cards_dealer_toshow = show_cards(hand_dealer)
 
     # draw two starting cards to the player
     hand_player = []
     draw_cards(hand_player, 2)
+    # calculate current player score
+    score_player = sum_hand(hand_player)
+    # get a reference to the list of cards in player's hand
+    cards_player_toshow = show_cards(hand_player)
 
     # game loop
     game_over = False
     while not game_over:
+        # calculate current player score
+        score_player = sum_hand(hand_player)
+        # get a reference to the updated list of cards in player's hand
+        cards_player_toshow = show_cards(hand_player)
+
         # show round summary to player
-        print(f"\n\t-> Your cards: {show_cards(hand_player)}, current score: {sum_hand(hand_player)}")
-        print(f"\n\t-> Dealer's first card: {hand_dealer[0][0]}")
+        print(f"\n\t-> Your cards: {cards_player_toshow}, current score: {score_player}")
+        print(f"\n\t-> Dealer's first card: {cards_dealer_toshow[0]}")
 
         # ask player input for draw or pass
         if input("\nType 'y' to draw another card, type 'n' to pass: ") == 'y':
             # draw another card to player
             draw_cards(hand_player)
+            # calculate new player score
+            score_player = sum_hand(hand_player)
+            # get a reference to the updated list of cards in player's hand
+            cards_player_toshow = show_cards(hand_player)
             # check it went over 21 -> bust, player loses -> game over
-            if sum_hand(hand_player) > 21:
-                print(f"\n\t-> Your cards: {show_cards(hand_player)}, score: {sum_hand(hand_player)}")
+            if score_player > 21:
+                print(f"\n\t-> Your cards: {cards_player_toshow}, score: {score_player}")
                 print('\n\t::: BUST! ::: YOU LOSE :::')
                 game_over = True
         
         else:
             # player pass -> show results -> game over
             # hands summary
-            print(f"\n\t-> Your cards: {show_cards(hand_player)}, score: {sum_hand(hand_player)}")
-            print(f"\n\t-> Dealer's cards: {show_cards(hand_dealer)}, score: {sum_hand(hand_dealer)}")
+            print(f"\n\t-> Your cards: {cards_player_toshow}, score: {score_player}")
+            print(f"\n\t-> Dealer's cards: {cards_dealer_toshow}, score: {score_dealer}")
 
             # player has blackjack and dealer not -> player wins
             if is_blackjack(hand_player) and not is_blackjack(hand_dealer):
@@ -53,10 +70,10 @@ def start_game():
             elif is_blackjack(hand_dealer) and not is_blackjack(hand_player):
                 print("\n\t::: DEALER'S BLACKJACK!!! YOU LOSE :::")
             # player with greater hand or dealer bust -> player win
-            elif sum_hand(hand_player) > sum_hand(hand_dealer) or sum_hand(hand_dealer) > 21:
+            elif score_player > score_dealer or score_dealer > 21:
                 print("\n\t::: YOU WIN! :::")
             # dealer with greater hand -> player lose
-            elif sum_hand(hand_player) < sum_hand(hand_dealer):
+            elif score_player < score_dealer:
                 print("\n\t::: YOU LOSE :::")
             # hands sums are equal -> draw
             else:
