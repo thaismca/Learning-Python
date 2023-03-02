324342f8e8c755ea26a122a4781380062844f21d
Thais Campos <thaismca.bio@gmail.com>
2023-03-01 18:42:04 -0800

    Project breakdown created project directory and defined the todos to break down the tasks required to implement this game

---
+++ Udemy - 100 days of Python/Intermediate sections/day-22_pong-game/main.py
@@ -0,0 +1,15 @@
+# TODO: create the screen where the game will be played
+
+# TODO: create and move paddle
+
+# TODO: create another paddle
+
+# TODO: create ball and make it move
+
+# TODO: detect collision with wall and bounce
+
+# TODO: detect collision with paddle and bounce
+
+# TODO: detect when paddle misses and the other player scores
+
+# TODO: keep and display score
