31b23bc185f7063788788a2acb52e42d2b37509a
Thais Campos <thaismca.bio@gmail.com>
2023-03-01 18:56:47 -0800

    Created screen screen where the game will be played is created

--- Udemy - 100 days of Python/Intermediate sections/day-22_pong-game/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-22_pong-game/main.py
@@ -1,4 +1,18 @@
 # TODO: create the screen where the game will be played
+from turtle import Screen
+
+# Settings
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.title("PONG")
+
+
+
+screen.exitonclick()
 
 # TODO: create and move paddle
 
