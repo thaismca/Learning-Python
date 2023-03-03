3f04b65b7399978719f6180311972ed0a7649b8f
Thais Campos <thaismca.bio@gmail.com>
2023-03-03 12:52:34 -0800

    Moved settings to separate file - game settings were moved to a separate file - screen size was changed to be pulled from settings

---
+++ Udemy - 100 days of Python/Intermediate sections/day-23_turtle-crossing-game/settings.py
@@ -0,0 +1,14 @@
+# Settings
+# screen
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+FINISH_LINE_Y = (SCREEN_HEIGHT/2) - 20
+
+# turtle player
+PLAYER_START_POSITION = (0, -(SCREEN_HEIGHT/2) + 20)
+PLAYER_MOVE_PACE = 10
+
+# cars
+CAR_COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+CAR_START_MOVE_PACE = 5
+CAR_MOVE_INCREMENT = 10
