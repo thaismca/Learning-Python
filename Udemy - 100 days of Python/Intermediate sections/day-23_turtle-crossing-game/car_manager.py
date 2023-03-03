492072cfaefb820aa3726e9d112d15123179274a
Thais Campos <thaismca.bio@gmail.com>
2023-03-03 15:49:51 -0800

    Implemented CarManager class - implemented a class that manages all cars that spawns in the screen - implemented method that renders a new car and adds it to the list of existing cars that are managed by the CarManager - implemented method that moves each car in the cars list forward at the manager's current pace - added the ability to spawn a new car every n-th time the game loop runs, n being a constant that represents the spawn interval in the game settings

--- Udemy - 100 days of Python/Intermediate sections/day-23_turtle-crossing-game/car_manager.py
+++ Udemy - 100 days of Python/Intermediate sections/day-23_turtle-crossing-game/car_manager.py
@@ -1,7 +1,31 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+from turtle import Turtle
+import random
+from settings import CAR_COLORS, CAR_START_MOVE_PACE, CAR_MOVE_INCREMENT, SPAWN_RANGE_MIN, SPAWN_RANGE_MAX, SCREEN_WIDTH
+
+class CarManager():
+    def __init__(self):
+        # list of existing cars
+        self.all_cars = []
+        # keeps track of pace
+        self.pace = CAR_START_MOVE_PACE
 
 
-class CarManager:
-    pass
+    def render_car(self):
+        """Renders a new car and adds it to the list of existing cars that are managed by this CarManager"""
+        new_car = Turtle()
+        new_car.shape("square")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.setheading(180)
+        new_car.penup()
+# TODO: add the ability to randomize car color
+        new_car.color(random.choice(CAR_COLORS))
+# TODO: add the ability to randomize the car spawn point in the y axis, excluding a safe zone at both bottom and top areas of the screen
+        new_car.goto(SCREEN_WIDTH/2 , random.randint(SPAWN_RANGE_MIN, SPAWN_RANGE_MAX))
+        self.all_cars.append(new_car)
+
+
+    def move(self):
+        """Moves each car in the cars list forward at the current pace"""
+        for car in self.all_cars:
+            car.forward(self.pace)
+
--- Udemy - 100 days of Python/Intermediate sections/day-23_turtle-crossing-game/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-23_turtle-crossing-game/main.py
@@ -4,7 +4,7 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FINISH_LINE_Y
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FINISH_LINE_Y, SPAWN_INTERVAL
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
@@ -19,10 +19,21 @@ screen.onkey(player.move_up, "Up")
 # TODO: create scoreboard that displays the player current level
 scoreboard = Scoreboard()
 
+# cars manager instance
+cars = CarManager()
+
 game_is_on = True
+loop_counter = SPAWN_INTERVAL
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # TODO: create a car 20px high by 40px wide that starts at the right edge of the screen and continuously moves forward
+    # TODO: add the ability to spawn a new car every n-th time the game loop runs, n being a constant that represents the spawn interval
+    if loop_counter == SPAWN_INTERVAL:
+        cars.render_car()
+        loop_counter = 1
+    cars.move()
 
     # TODO: add the ability to check when turtle reaches the top of the screen
     if player.ycor() >= FINISH_LINE_Y:
@@ -31,16 +42,12 @@ while game_is_on:
     # TODO: increase player level and refresh scoreboard when turtle reaches the top of the screen
         scoreboard.increase_level()
 
+    loop_counter += 1
+
 
 screen.exitonclick()
 
 
-
-
-# TODO: create a car 20px high by 40px wide that starts at the right edge of the screen and continuously moves forward
-# TODO: add the ability to randomize the car spawn point in the y axis, excluding a safe zone at both bottom and top areas of the screen
-# TODO: add the ability to spawn a new car every n-th time the game loop runs, n being a constant that represents the spawn rate
-# TODO: add the ability to randomize car color
 # TODO: add the ability to increase car speed at each level
 # TODO: add the ability to detect collision between a car and turtle
 # TODO: add the ability to display a game over message in the scoreboard when a collision between car and turtle is detected
