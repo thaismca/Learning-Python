848e8a767f9612e5fea1fa5d3a99d28a4c3bad89
Thais Campos <thaismca.bio@gmail.com>
2023-03-04 15:16:13 -0800

    Starting mail merge project - set up project directory structure - created starting files in the input folder - broken-down the problem in todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-24_mail-merge/main.py
@@ -0,0 +1,3 @@
+# TODO: create letter using starting_letter.txt
+# TODO: for each name in invited_names.txt, replace [name] placeholder with the actual name
+# TODO: save letters in the output folder
