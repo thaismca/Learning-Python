6e762dcf6f898ca33ca570b1e650df03d7b08573
Thais Campos <thaismca.bio@gmail.com>
2023-03-04 15:48:41 -0800

    Completed day 24 mail merge challenge - cleaned up todos from code - ran program and generated letters in the output folder

--- Udemy - 100 days of Python/Intermediate sections/day-24_mail-merge/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-24_mail-merge/main.py
@@ -2,11 +2,11 @@ TEMPLATE_PATH = "./Intermediate sections/day-24_mail-merge/input/starting_letter
 NAMES_LIST = "./Intermediate sections/day-24_mail-merge/input/invited_names.txt"
 OUTPUT_PATH = "./Intermediate sections/day-24_mail-merge/output/"
 
-# TODO: create letter using starting_letter.txt
+# create letter using starting_letter.txt
 with open(TEMPLATE_PATH) as template:
     letter = template.read()
 
-# TODO: for each name in invited_names.txt, replace [name] placeholder with the actual name
+# for each name in invited_names.txt, replace [name] placeholder with the actual name
 # Python File readlines() Method: return all lines in the file, as a list where each line is an item in the list object
 # Python String replace() Method: replaces all occurences of occurrences of a specified phrase with another specified phrase
 
@@ -14,7 +14,7 @@ with open(NAMES_LIST) as names:
     names_list = names.readlines()
 
 for name in names_list:
-    # TODO: save letters in the output folder
+    # save letters in the output folder
     output_file_name = "letter_for_" + name.strip().lower() + ".txt"
     with open(OUTPUT_PATH + output_file_name, mode="w") as new_letter:
         new_letter.write(letter.replace("[name]", name.strip()))
