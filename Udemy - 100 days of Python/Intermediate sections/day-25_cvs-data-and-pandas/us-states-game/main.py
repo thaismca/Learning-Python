f6f66622075b2a3c5be301668bd2beca7676c9cf
Thais Campos <thaismca.bio@gmail.com>
2023-03-05 14:35:08 -0800

    Generate states list - added code to read states from 50_states.csv and generate a list with all the 50 states

--- Udemy - 100 days of Python/Intermediate sections/day-25_cvs-data-and-pandas/us-states-game/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-25_cvs-data-and-pandas/us-states-game/main.py
@@ -1,4 +1,8 @@
 import turtle
+import pandas
+
+# path for the csv file
+DATA_FILE_PATH = "./Intermediate sections/day-25_cvs-data-and-pandas/us-states-game/50_states.csv"
 
 # create screen
 screen = turtle.Screen()
@@ -10,9 +14,11 @@ screen.addshape(bg_image)
 turtle.shape(bg_image)
 
 #create pop-up input to get player's answer
-answer = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+# answer = screen.textinput(title="Guess the state", prompt="What's another state's name?")
 
 # TODO: read states from 50_states.csv and generate a list with all the 50 states
+data = pandas.read_csv(DATA_FILE_PATH)
+states = data.state
 # TODO: while there are still states to be guessed, display a pop up input to receive a player's answer
 # TODO: display current score in the title of the pop up input
 # TODO: check if player's answer matches one of the states in the 50_states.csv
