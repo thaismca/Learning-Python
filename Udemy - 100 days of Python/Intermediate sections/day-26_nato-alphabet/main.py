0e36c3de7339dcb3f7f5e1032a1e8d43e6a97a40
Thais Campos <thaismca.bio@gmail.com>
2023-03-09 15:30:17 -0800

    Finished NATO converter project - cleaned up todos and fixed some spelling errors in comments - added documentation

--- Udemy - 100 days of Python/Intermediate sections/day-26_nato-alphabet/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-26_nato-alphabet/main.py
@@ -3,14 +3,13 @@
 import pandas
 NATO_CSV_FILEPATH = './Intermediate sections/day-26_nato-alphabet/nato_phonetic_alphabet.csv'
 
-# TODO: convert the nato_phonetic_alphabet.csv file to a dataframe
+# convert the nato_phonetic_alphabet.csv file to a dataframe
 df_nato = pandas.read_csv(NATO_CSV_FILEPATH)
-# TODO: convert the dataframe to a dictionary, where each letter is a key and the corresponding code is the value
+# convert the dataframe to a dictionary, where each letter is a key and the corresponding code is the value
 dict_nato = {row.letter:row.code for (index, row) in df_nato.iterrows()}
-print(dict_nato)
-# TODO: receive string from user -> remove any spaces, because only characters will matter -> convert to uppercase
+# receive string from user -> remove any spaces, because only characters will matter -> convert to uppercase
 word_to_spell = input("Enter a word: ").replace(" ", "").upper()
-# TODO: for each character, use the nato alphabeth dictionary to find the corresponding code -> append this code to a result list
+# for each character, use the nato alphabet dictionary to find the corresponding code -> append this code to a result list
 result = [dict_nato[char] for char in word_to_spell if char in dict_nato.keys()]
-# TODO: output the result list
+# output the result list
 print(result)
