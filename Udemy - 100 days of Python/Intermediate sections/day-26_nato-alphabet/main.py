61408bf8b884d844a3c9688412449006a4b81fbb
Thais Campos <thaismca.bio@gmail.com>
2023-03-09 15:02:59 -0800

    Receiving and treating user input - added code to receive string from user -> remove any spaces, because only characters will matter -> convert to uppercase - converted that resulting string to a list of characters

--- Udemy - 100 days of Python/Intermediate sections/day-26_nato-alphabet/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-26_nato-alphabet/main.py
@@ -8,8 +8,11 @@ df_nato = pandas.read_csv(NATO_CSV_FILEPATH)
 # TODO: convert the dataframe to a dictionary, where each letter is a key and the corresponding code is the value
 dict_nato = {row.letter:row.code for (index, row) in df_nato.iterrows()}
 print(dict_nato)
-# TODO: receive string from user
+# TODO: receive string from user -> remove any spaces, because only characters will matter -> convert to uppercase
+word_to_spell = input("Enter a word: ").replace(" ", "").upper()
 # TODO: convert string to list of characters
+word_chars = [char for char in word_to_spell]
+print(word_chars)
 # TODO: for each character, use the nato alphabeth dictionary to find the corresponding code
         # TODO: append a string "{character} for {code}" to a result list
 # TODO: output the result list
