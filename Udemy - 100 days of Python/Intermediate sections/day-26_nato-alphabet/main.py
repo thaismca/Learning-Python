31346e1eb5f59a6472473f15af99fa92f149b2d0
Thais Campos <thaismca.bio@gmail.com>
2023-03-09 12:22:04 -0800

    Starting day 26 project - nato alphabet spelling -added csv file containing letters and their respective codes - broke down project in todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-26_nato-alphabet/main.py
@@ -0,0 +1,9 @@
+# the following code receives a string and outputs it spelled using the nato phonetic alphabet
+
+# TODO: convert the nato_phonetic_alphabet.csv file to a dataframe
+# TODO: convert the dataframe to a dictionary, where each letter is a key and the corresponding code is the value
+# TODO: receive string from user
+# TODO: convert string to list of characters
+# TODO: for each character, use the nato alphabeth dictionary to find the corresponding code
+        # TODO: append a string "{character} for {code}" to a result list
+# TODO: output the result list
