b26a10a77bf69757a2a4d8d294ac3e31cdcb2640
Thais Campos <thaismca.bio@gmail.com>
2023-03-10 16:02:38 -0800

    Day 27 project setup - determined project goal - broke down project in todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-27_GUI-programs/road_runner_calculator/main.py
@@ -0,0 +1,14 @@
+# course project: create program that converts miles to kilometers
+# this project: converts units for a value based on user selection -> made for road runners
+# options available:
+# distance - miles and kilometers
+# speed - miles per hour and kilometers per hour
+# pace - minutes per mile and minutes per kilometer
+
+# TODO: create a tkinter window
+# TODO: create a radiobutton for the conversion types options (distance = default selection, speed or pace)
+# TODO: create a selection for the base unit that changes its options based on conversion type selecion
+# TODO: create input for base value to be converted
+# TODO: implement conversion calculation formulas
+# TODO: create button that applies calculation formula based on conversion options selected
+# TODO: create result label that displayes result when button is clicked 
