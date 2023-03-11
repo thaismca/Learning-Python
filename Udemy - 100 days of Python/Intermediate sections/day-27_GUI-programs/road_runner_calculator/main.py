bc2224289dabe1a1a4c2a65b3212444c6efaa3e4
Thais Campos <thaismca.bio@gmail.com>
2023-03-10 16:19:38 -0800

    Conversion type selection - created a radiobutton selection for the conversion types options (distance = default selection, speed or pace)

--- Udemy - 100 days of Python/Intermediate sections/day-27_GUI-programs/road_runner_calculator/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-27_GUI-programs/road_runner_calculator/main.py
@@ -11,11 +11,21 @@ window = Tk()
 window.title("The road runner conversion calculator")
 window.minsize(width=500, height=250)
 
+# TODO: create a radiobutton selection for the conversion types options (distance = default selection, speed or pace)
+conversion_type_state = StringVar(None, "distance")
+distance = Radiobutton(text="Distance", value="distance", variable=conversion_type_state)
+speed = Radiobutton(text="Speed", value="speed", variable=conversion_type_state)
+pace = Radiobutton(text="Pace", value="pace", variable=conversion_type_state)
+
+distance.grid(row=0, column=0)
+speed.grid(row=0, column=1)
+pace.grid(row=0, column=2)
+
 
 # keep the window open and listening for user interactions
 window.mainloop()
 
-# TODO: create a radiobutton for the conversion types options (distance = default selection, speed or pace)
+
 # TODO: create a selection for the base unit that changes its options based on conversion type selecion
 # TODO: create input for base value to be converted
 # TODO: implement conversion calculation formulas
