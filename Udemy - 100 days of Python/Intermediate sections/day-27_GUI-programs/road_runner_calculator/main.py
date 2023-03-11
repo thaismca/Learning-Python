0a51964d7fc7b5c8c96541a54b4732b401dce256
Thais Campos <thaismca.bio@gmail.com>
2023-03-10 16:06:33 -0800

    Window created - created a tkinter window where the program will run

--- Udemy - 100 days of Python/Intermediate sections/day-27_GUI-programs/road_runner_calculator/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-27_GUI-programs/road_runner_calculator/main.py
@@ -5,7 +5,16 @@
 # speed - miles per hour and kilometers per hour
 # pace - minutes per mile and minutes per kilometer
 
+from tkinter import *
 # TODO: create a tkinter window
+window = Tk()
+window.title("The road runner conversion calculator")
+window.minsize(width=500, height=250)
+
+
+# keep the window open and listening for user interactions
+window.mainloop()
+
 # TODO: create a radiobutton for the conversion types options (distance = default selection, speed or pace)
 # TODO: create a selection for the base unit that changes its options based on conversion type selecion
 # TODO: create input for base value to be converted
