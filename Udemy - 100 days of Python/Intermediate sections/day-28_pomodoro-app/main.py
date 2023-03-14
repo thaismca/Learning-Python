af1aba35e4cc7b866dcfbbab486bc52b6db4184a
Thais Campos <thaismca.bio@gmail.com>
2023-03-14 15:18:18 -0700

    Project setup - added image that it will be used in the application background - created constants - broke down the project in todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-28_pomodoro-app/main.py
@@ -0,0 +1,29 @@
+
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+WORK_SESSIONS_PER_BLOCK: 4
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# TODO: set up UI with:
+# a background image
+# countdown label
+# session label (break/work)
+# work sessions counter (checkmarks)
+# start and reset buttons
+
+# TODO: add countdown mechanism
+
+# TODO: set different timer sesisons and values
+
+# TODO: display checkmarks when each work session is done
+# TODO: reset the work sessions counter when the block reaches a number of work sessions
+# (pomodoro applies blocks with 4 work sessions, but here it can be customized)
+
+# TODO: add functionality to reset the whole application when clicking the reset button
