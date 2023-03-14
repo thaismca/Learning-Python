32de1e9a20618b9d5ceed358fee182a6b6164010
Thais Campos <thaismca.bio@gmail.com>
2023-03-14 16:31:12 -0700

    UI setup - created a canvas with a background image and a label that will hold the countdown on the top layer - created a session label that will hold either 'break' or 'work' text - created start and reset buttons - created the label that will hold that checkmarks that will count the number of sessions in the block

--- Udemy - 100 days of Python/Intermediate sections/day-28_pomodoro-app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-28_pomodoro-app/main.py
@@ -7,16 +7,46 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
-WORK_SESSIONS_PER_BLOCK: 4
+WORK_SESSIONS_PER_BLOCK = 4
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+BG_IMAGE = './Intermediate sections/day-28_pomodoro-app/tomato.png'
 
 # TODO: set up UI with:
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# session label (break/work)
+session_label = Label(text='SESSION', font=(FONT_NAME, 40, "bold"), fg=GREEN, bg=YELLOW, pady=20)
+
 # a background image
 # countdown label
-# session label (break/work)
-# work sessions counter (checkmarks)
+#create a canvas with the size of the image
+canvas = Canvas(width=204, height=224, bg=YELLOW, highlightthickness=0)
+# use the PhotoImage widget to display the image
+tomato_image = PhotoImage(file=BG_IMAGE)
+# add image to the canvas in the passed coordinates (anchor point in the center of the item)  
+canvas.create_image(102, 112, image=tomato_image)
+# add text to the canvas in the passed coordinates -> order matters, later goes on top
+canvas.create_text(102, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+
 # start and reset buttons
+start_button = Button(text='start')
+reset_button = Button(text='reset')
+
+# work sessions counter (checkmarks)
+sessions_counter_label = Label(text='✔ ✔ ✔ ✔', font=(FONT_NAME, 20, "normal"), fg=GREEN, bg=YELLOW, pady=20)
+
+# pace items on screen
+session_label.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
+start_button.grid(row=2, column=0)
+reset_button.grid(row=2, column=2)
+sessions_counter_label.grid(row=3, column=1)
+
+
+window.mainloop()
 
 # TODO: add countdown mechanism
 
