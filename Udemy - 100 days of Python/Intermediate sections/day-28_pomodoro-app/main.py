0908ee2c05f654f5188cef8b242bc146b02b1ee5
Thais Campos <thaismca.bio@gmail.com>
2023-03-15 10:52:32 -0700

    Added countdown mechanism - created countdown function, that displays the current time remaining in the format MM:SS, based on the number of seconds passed as argument. If there are still time remaining (seconds > 0), the function calls itself after one second, passing (seconds - 1) as argument. - created a start_timer function, that takes a number of minutes and seconds, calculates the number of seconds in these minutes and seconds, and starts a countdown passing the number of total seconds. - added a temporary testing call for the start_timer, to test the countdown behaviour.

--- Udemy - 100 days of Python/Intermediate sections/day-28_pomodoro-app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-28_pomodoro-app/main.py
@@ -12,6 +12,33 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 BG_IMAGE = './Intermediate sections/day-28_pomodoro-app/tomato.png'
 
+
+# TODO: add countdown mechanism
+# ---------------------------------- TIMER MECHANISM --------------------------------- #
+def start_timer(min=0, sec=0):
+    '''This function takes a number of minutes and seconds, calculates the number of seconds in these minutes and seconds,
+    and starts a countdown passing the number of total seconds'''
+    total_seconds = (min * 60) + sec
+    countdown(total_seconds)
+
+
+# -------------------------------- COUNTDOWN MECHANISM ------------------------------- #
+import datetime
+
+def countdown(seconds):
+    '''This function displays the current time remaining in the format MM:SS, based on the number of seconds passed as argument.
+    If there are still time remaining (seconds > 0), the function calls itself after one second, passing (seconds - 1) as argument.'''
+    # display timer in the MM:SS format, passing the number of seconds
+    time_remaining = str(datetime.timedelta(seconds=seconds))[-5:]
+    canvas.itemconfig(timer_text, text=time_remaining)
+
+    #if seconds is greater than 0, call this function again after one second, passing (seconds - 1) as argument
+    if seconds > 0:
+        window.after(1000, countdown, seconds - 1)
+
+
+# ------------------------------------- UI SETUP ------------------------------------- #
+
 # TODO: set up UI with:
 window = Tk()
 window.title("Pomodoro")
@@ -29,7 +56,7 @@ tomato_image = PhotoImage(file=BG_IMAGE)
 # add image to the canvas in the passed coordinates (anchor point in the center of the item)  
 canvas.create_image(102, 112, image=tomato_image)
 # add text to the canvas in the passed coordinates -> order matters, later goes on top
-canvas.create_text(102, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(102, 130, text="00:00", fill='white', font=(FONT_NAME, 24, 'bold'))
 
 # start and reset buttons
 start_button = Button(text='start')
@@ -45,12 +72,13 @@ start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=2)
 sessions_counter_label.grid(row=3, column=1)
 
+# testing countdown behaviour
+start_timer(0, 15)
 
 window.mainloop()
 
-# TODO: add countdown mechanism
 
-# TODO: set different timer sesisons and values
+# TODO: set different timer sessions and values
 
 # TODO: display checkmarks when each work session is done
 # TODO: reset the work sessions counter when the block reaches a number of work sessions
