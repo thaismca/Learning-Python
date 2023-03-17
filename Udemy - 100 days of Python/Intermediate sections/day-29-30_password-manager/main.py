0fe181cdc204b95020225f96f6e4d5b1dc683af4
Thais Campos <thaismca.bio@gmail.com>
2023-03-17 13:00:56 -0700

    Start window setup - created window where the program will run - added a canvas containing the logo image

--- Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
@@ -1,5 +1,5 @@
 # --- PASSWORD MANAGER --- #
-# create a windown where the program will run
+# create a window where the program will run
 # add logo image
 # create entry for website name
 # create entry for username
@@ -10,3 +10,21 @@
 # create error message pop up -> error message label and ok button to dismiss the pop up and return to the main window
 # create confirmation pop up -> data label, cancel button that simply closes the pop up with no further actions, and save button
 # implement behaviour for save button in the confirmation pop up -> save data to a data file and clear the form in the main window 
+
+from tkinter import *
+
+LOGO_IMAGE = 'logo.png'
+
+# ------------------------------ UI SETUP --------------------------------- #
+# create a window where the program will run
+window = Tk()
+window.config(padx=20)
+
+# add logo image
+logo_canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file=LOGO_IMAGE)
+logo_canvas.create_image(100, 100, image=logo_image)
+logo_canvas.pack()
+
+
+window.mainloop()
