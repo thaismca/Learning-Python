62314695c9385b264aa5433197dd9d4c75fd6980
Thais Campos <thaismca.bio@gmail.com>
2023-03-17 12:46:24 -0700

    Project setup - added image file with the program logo - broke down project in todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
@@ -0,0 +1,12 @@
+# --- PASSWORD MANAGER --- #
+# create a windown where the program will run
+# add logo image
+# create entry for website name
+# create entry for username
+# create entry for password
+# create generate password button and implement generate password functionality
+# create add button to submit the form
+# implement submit form where it checks for empty inputs and display either error message or confirmation message in pop up
+# create error message pop up -> error message label and ok button to dismiss the pop up and return to the main window
+# create confirmation pop up -> data label, cancel button that simply closes the pop up with no further actions, and save button
+# implement behaviour for save button in the confirmation pop up -> save data to a data file and clear the form in the main window 
