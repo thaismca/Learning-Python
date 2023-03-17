f7f5e56e8928cd4b2fc2a08bfbeb0d66c60af3fc
Thais Campos <thaismca.bio@gmail.com>
2023-03-17 16:16:40 -0700

    User interface setup - created label and entry for website name - created label and entry for username - created label and entry for password - created generate password button - created add button

--- Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
@@ -18,13 +18,39 @@ LOGO_IMAGE = 'logo.png'
 # ------------------------------ UI SETUP --------------------------------- #
 # create a window where the program will run
 window = Tk()
-window.config(padx=20)
+window.config(padx=30, pady=30)
 
 # add logo image
 logo_canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file=LOGO_IMAGE)
 logo_canvas.create_image(100, 100, image=logo_image)
-logo_canvas.pack()
+logo_canvas.grid(row=0, column=1)
+
+# website name
+website_label = Label(text='Website:', pady=5)
+website_entry = Entry()
+website_label.grid(row=1, column=0)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+
+# username
+username_label = Label(text='Email/Username:', pady=5)
+username_entry = Entry()
+username_label.grid(row=2, column=0)
+username_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+
+# password
+password_label = Label(text='Password:', pady=5)
+password_entry = Entry()
+password_label.grid(row=3, column=0)
+password_entry.grid(row=3, column=1, sticky="EW")
+
+# generate password
+gen_password_button = Button(text='Generate Password')
+gen_password_button.grid(row=3, column=2, sticky="EW")
+
+# add -> submit
+add_button = Button(text='Add', width=35)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 
 window.mainloop()
