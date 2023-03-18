f5a9e0d3655219b7278bbee529fa5e8ca75727c6
Thais Campos <thaismca.bio@gmail.com>
2023-03-17 21:44:32 -0700

    Generate password - added function that generates a new random password, adds it to the password entry in the form, and copies the generated password to the clipboard. - attached the generate_password function to the 'generate password' button, as its command property

--- Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
@@ -1,21 +1,12 @@
 # --- PASSWORD MANAGER --- #
-# create a window where the program will run
-# add logo image
-# create entry for website name
-# create entry for username
-# create entry for password
-# create generate password button and implement generate password functionality
-# create add button to submit the form
-# implement submit form where it checks for empty inputs and display either error message or confirmation message in pop up
-# create error message pop up -> error message label and ok button to dismiss the pop up and return to the main window
-# create confirmation pop up -> data label, cancel button that simply closes the pop up with no further actions, and save button
-# implement behaviour for save button in the confirmation pop up -> save data to a data file and clear the form in the main window 
 
 from tkinter import *
 from tkinter import messagebox
 
 LOGO_IMAGE = 'logo.png'
-
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 # ---------------------------- SAVE PASSWORD ------------------------------ #
 def submit_entries():
@@ -33,7 +24,7 @@ def submit_entries():
 
 
 def confirm_save(website, username, password):
-    '''Displays a message to confirm save containing the data submit in the form so user can confer. Saves the data only if user clicks 'yes'.'''
+    '''Displays a messagebox to confirm save containing the data submitted in the form so user can confer. Saves the data only if user clicks 'yes'.'''
     confirm_message = f"Website: {website}\nUsername/Email: {username}\nPassword: {password}\n\nConfirm save?"
     confirmation = messagebox.askyesno(title='Confirm save', message=confirm_message)
     if confirmation:
@@ -73,6 +64,27 @@ def clear_form_entries():
         
 
 
+# -------------------------- PASSWORD GENERATOR --------------------------- #
+from random import randint, choice, shuffle
+import pyperclip
+def generate_password():
+    '''Generates a new random password, adds it to the password entry in the form, and copies the generated password to the clipboard.'''
+    password_letters = [choice(LETTERS) for _ in range(randint(8, 10))]
+    password_symbols = [choice(SYMBOLS) for _ in range(randint(2, 4))]
+    password_numbers = [choice(NUMBERS) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = ''.join(password_list)
+
+    # display generated password in password entry
+    password_entry.insert(0, password)
+    # copy generated password to clipboard
+    pyperclip.copy(password)
+
+
+
 # ------------------------------ UI SETUP --------------------------------- #
 # create a window where the program will run
 window = Tk()
@@ -105,7 +117,7 @@ password_label.grid(row=3, column=0)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # generate password
-gen_password_button = Button(text='Generate Password')
+gen_password_button = Button(text='Generate Password', command=generate_password)
 gen_password_button.grid(row=3, column=2, sticky="EW")
 
 # add -> submit
