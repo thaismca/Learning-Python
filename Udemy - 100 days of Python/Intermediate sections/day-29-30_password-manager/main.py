b5cf1b7d5d4dc3987b2f52173b483c07a8663e60
Thais Campos <thaismca.bio@gmail.com>
2023-03-17 17:10:52 -0700

    Save data functionality - added a function that saves the data entered in the password manager form to a data.txt file, making sure that the entries in the data.txt file are sorted alphabetically. - attached the save_password functionality to the 'add' button

--- Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-29-30_password-manager/main.py
@@ -15,6 +15,35 @@ from tkinter import *
 
 LOGO_IMAGE = 'logo.png'
 
+# ---------------------------- SAVE PASSWORD ------------------------------ #
+def save_password():
+    '''Saves the data entered in the password manager form to a data.txt file.
+    The entries in the data.txt file will be sorted alphabetically.'''
+    # get data from inputs
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # form the new data entry
+    new_data = f"{website} | {username} | {password}\n"
+    
+    data_list = []
+    try:
+        # try to open an existing data file and read data from it
+        with open("data.txt", "r") as file:
+            data_list = file.readlines()
+    except:
+        pass
+    
+    # add the new data to the existing list
+    data_list.append(new_data)
+    data_list.sort()
+
+    # overwrite the file with the updated list
+    with open("data.txt", "w") as file:
+        file.writelines(data_list)
+        
+
 # ------------------------------ UI SETUP --------------------------------- #
 # create a window where the program will run
 window = Tk()
@@ -29,6 +58,7 @@ logo_canvas.grid(row=0, column=1)
 # website name
 website_label = Label(text='Website:', pady=5)
 website_entry = Entry()
+website_entry.focus()
 website_label.grid(row=1, column=0)
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 
@@ -49,7 +79,7 @@ gen_password_button = Button(text='Generate Password')
 gen_password_button.grid(row=3, column=2, sticky="EW")
 
 # add -> submit
-add_button = Button(text='Add', width=35)
+add_button = Button(text='Add', width=35, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 
