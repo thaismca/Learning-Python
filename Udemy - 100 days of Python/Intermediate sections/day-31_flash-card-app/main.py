d89bea6f69f088e2f07eb39ae3616f30dd00bbfd
Thais Campos <thaismca.bio@gmail.com>
2023-03-21 11:53:47 -0700

    Flip card implementation and changes to data reading - defined global variables that will represent the app data list, the selected card for each time a new card is generated and the languages that the app is currently working with. - created a function that will handle data reading from a csv and creation of the data lists of words dictionaries and current languages - to allow different language selection in future iteration - modified generate_card to account for a possible programmed delay, apply more changes to the window elements and to call the function to flip the card after a given amount of time - implemented the function that shows the back of the selected card - modified some variable names for better readability

--- Udemy - 100 days of Python/Intermediate sections/day-31_flash-card-app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-31_flash-card-app/main.py
@@ -15,29 +15,77 @@ WORD_LABEL_FONT = ('Arial', 60, 'bold')
 # -- DATA FILE
 DATA_FILE = './data/french_words.csv'
 
-# TODO: create new flash cards
-from csv import DictReader
+# -- SETTINGS
+FLIP_DELAY = 3000
+
+
+# ----------------- GLOBAL VARIABLES ---------------- #
+words_to_learn = []
+selected_card = {}
+languages = []
+
+
+# ----------------- READ APP DATA ------------------ #
+def read_app_data():
+    from csv import DictReader
+    # read from the csv file to dictionary
+    with open(DATA_FILE, mode='r', encoding='utf-8-sig') as data_file:
+        global words_to_learn
+        global languages
+        words_to_learn = list(DictReader(data_file))
+        # get a reference to the name of the languages
+        languages = [language for language in words_to_learn[0].keys()]
+    
+
+# -------------- GENERATE / FLIP CARDS -------------- #
 import random
-# read from the csv file to dictionary
-with open(DATA_FILE, mode='r', encoding='utf-8-sig') as data_file:
-    words_dict = list(DictReader(data_file))
-
-
+# TODO: create new flash cards
 # create a card front with a random word in the first column
 # generate a new card every time a right or wrong button is pressed
 # show the front of the card whenever a new card is generated
 def generate_card(dict_list):
     '''Receives a list of dictionaties, where each dictionary has a word in two different languages in the format
-    {language1: word, language2:translation}. It selects a randon dictionary in the list and generates the front of a flash card.'''
+    {language1: word, language2:translation}. It selects a random dictionary in the list and generates the front of a flash card.
+    Calls flip_card to show the back of the card after the amount of time defined in the FLIP_DELAY constant.'''
+    try:
+        # try to cancel a previous delay, if there is one
+        window.after_cancel(flip)
+    except:
+        pass
     # select random word/translation dict
-    selected = random.choice(dict_list)
-    # get a reference to the name of the languages
-    languages = [language for language in selected.keys()]
+    global selected_card
+    global languages
+    selected_card = random.choice(dict_list)
     # change card label text for current language
-    card.itemconfig(current_language, text=languages[0])
+    card.itemconfig(card_language, text=languages[0], fill='#000')
     # change card label text for current language
-    card.itemconfig(current_word, text=selected[languages[0]])
+    card.itemconfig(card_word, text=selected_card[languages[0]], fill='#000')
+    # change card background image
+    card.itemconfig(card_bg, image=card_front_bg)
+    # disable buttons so they cannot be clicked while the front of the card is being displayed
+    right_button.config(state='disabled')
+    wrong_button.config(state='disabled')
 
+    flip = window.after(FLIP_DELAY, func= lambda: flip_card(selected_card))
+
+
+
+# TODO: after a delay of n seconds, flip the card to display the back with the translation
+# change card bg image
+# change card language label
+# change card word label
+# change both labels fg color
+def flip_card(selected_card):
+    '''Shows the back of a flash card. Called by generate_card after some amount of time defined in the FLIP_DELAY constant.'''
+    # change card label text for current language
+    card.itemconfig(card_language, text=languages[1], fill='#FFF')
+    # change card label text for current language
+    card.itemconfig(card_word, text=selected_card[languages[1]], fill='#FFF')
+    # change card backgroud image
+    card.itemconfig(card_bg, image=card_back_bg)
+    # enable buttons so they can be clicked while back of the card is being displayed
+    right_button.config(state='normal')
+    wrong_button.config(state='normal')
 
 
 # -------------------- UI SETUP --------------------- #
@@ -48,33 +96,33 @@ window = Tk()
 window.title('Flash Card App')
 window.config(bg=BG_COLOR, padx=50, pady=50)
 
+# read app data
+read_app_data()
+
 # create card with bg image, one label for the language name, and one label for the word
 card = Canvas(width=800, height=526, bg=BG_COLOR, highlightthickness=0)
 card_front_bg = PhotoImage(file=CARD_FRONT_IMG)
-card.create_image(400, 263, image=card_front_bg)
-current_language = card.create_text(400, 150, text='Language', font=LANGUAGE_LABEL_FONT)
-current_word = card.create_text(400, 263, text='Word', font=WORD_LABEL_FONT)
+card_back_bg = PhotoImage(file=CARD_BACK_IMG)
+card_bg = card.create_image(400, 263, image=card_front_bg)
+card_language = card.create_text(400, 150, text='Language', font=LANGUAGE_LABEL_FONT)
+card_word = card.create_text(400, 263, text='Word', font=WORD_LABEL_FONT)
 card.grid(row=0, column=0, columnspan=2)
 
 # create the buttons for right and wrong answer
 right_img = PhotoImage(file=RIGHT_BUTTON_IMG)
-right_button = Button(image=right_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command= lambda: generate_card(words_dict))
+right_button = Button(image=right_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command= lambda: generate_card(words_to_learn))
 right_button.grid(row=1, column=0)
 
 wrong_img = PhotoImage(file=WRONG_BUTTON_IMG)
-wrong_button = Button(image=wrong_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR)
+wrong_button = Button(image=wrong_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command= lambda: generate_card(words_to_learn))
 wrong_button.grid(row=1, column=1)
 
+# generate first card
+generate_card(words_to_learn)
 
 window.mainloop()
 
 
-
-# TODO: after a delay of n seconds, flip the card to display the back with the translation
-# change card bg image
-# change card language label
-# change card word label
-# change both labels fg color
 
 # TODO: save progress
 # remove word from list of words that might come up, if right button is pressed
