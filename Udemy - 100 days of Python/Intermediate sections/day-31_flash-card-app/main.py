da4af4857320946d753d5ec791b0d43b53286774
Thais Campos <thaismca.bio@gmail.com>
2023-03-22 20:24:32 -0700

    Working with multiple languages - added more languages supported by the app and the respective files to run the program for each one of them (portuguese and spanish) - added functionality that allows user to select a language to learn in the app - refactored some of the existing code and changed variable names for better readability

--- Udemy - 100 days of Python/Intermediate sections/day-31_flash-card-app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-31_flash-card-app/main.py
@@ -1,4 +1,9 @@
 # ----- FLASH CARD APP ----- #
+from tkinter import *
+from tkinter import messagebox
+import csv
+import os
+import random
 
 # -------------------- CONSTANTS -------------------- #
 # -- BACKGROUNDS
@@ -12,59 +17,98 @@ WRONG_BUTTON_IMG = './images/wrong.png'
 LANGUAGE_LABEL_FONT = ('Arial', 30, 'italic')
 WORD_LABEL_FONT = ('Arial', 60, 'bold')
 
-# -- DATA FILE
-DATA_FILE = './data/french_words.csv'
-SAVE_FILE = './data/words_to_learn.csv'
-
 # -- SETTINGS
-FLIP_DELAY = 1000
+FLIP_DELAY = 4000
+LANGUAGES_TO_PRACTICE = ['French', 'Portuguese', 'Spanish']
 
 
 # ----------------- GLOBAL VARIABLES ---------------- #
+all_words_file = ''
+saved_progress_file = ''
 words_to_learn = []
 selected_card = {}
-languages = []
+current_languages = []
 
 
-# ----------------- READ APP DATA ------------------ #
-import csv
-from tkinter import messagebox
+# ----------------- CHOOSE LANGUAGE ------------------ #
+def choose_language_pop_up(root):
+    '''Displays a pop up where the user can select a language to practice.'''
+    # pop up
+    pop_up = Toplevel(root)
+    pop_up.attributes('-topmost', True)
+    pop_up.focus_force()
+    # label and options menu
+    Label(pop_up, text="Choose language to practice").grid(row=0, column=0)
+    language_selected = StringVar()
+    language_menu = OptionMenu(pop_up, variable=language_selected, value='')
+    
+    #populate language options
+    menu = language_menu['menu'] 
+    for language in LANGUAGES_TO_PRACTICE:
+        menu.add_command(label=language, command=lambda lang=language: language_selected.set(lang))
+    # language selected by default
+    language_selected.set(LANGUAGES_TO_PRACTICE[0])
+    language_menu.grid(row=1, column=0)
+    
+    # button that calls set_language passing the selected language when clicked
+    Button(pop_up, text='OK', command= lambda: set_language(language_selected.get(), pop_up)).grid(row=2, column=0)
 
+
+
+def set_language(language, pop_up):
+    '''Sets the path to reach the program files for the language that was selected and calls star_app to read
+    either saved or base data from the corresponding language.'''
+    # tap into the global variables for saved base file paths
+    global all_words_file
+    global saved_progress_file
+
+    # set the paths according to the selected language
+    all_words_file = f'./data/{language.lower()}/all_words.csv'
+    saved_progress_file = f'./data/{language.lower()}/words_to_know.csv'
+
+    # close pop up and start_app
+    pop_up.destroy()
+    start_app()
+
+
+
+# ------------------ READ APP DATA ------------------- #
 def start_app():
-    '''Reads data from a save or base file and initialzes words_to_learn and languages globals.
+    '''Reads data from a save or base file and initializes words_to_learn and languages globals.
     Calls restart_words if there are no words to learn, or generate_card to show the first card if there are words to learn.
     Displays an error message if no file can be found.'''
+    # tap into the global variables for words to learn and languages
     global words_to_learn
-    global languages
+    global current_languages
     try:
-        # TODO: check for progress when open the app
-        # tries to read from the save file to dictionary
-        with open(SAVE_FILE, mode='r', encoding='utf-8-sig') as data_file:
+        # check for progress when open the app
+        # try to read from the save file to dictionary
+        with open(saved_progress_file, mode='r', encoding='utf-8-sig') as data_file:
             words_to_learn = list(csv.DictReader(data_file))
     except:
         try:
-            # tries read from a base data csv file with all the words to dictionary
-            with open(DATA_FILE, mode='r', encoding='utf-8-sig') as data_file:
+            # try to read from a base data csv file with all the words to dictionary
+            with open(all_words_file, mode='r', encoding='utf-8-sig') as data_file:
                 words_to_learn = list(csv.DictReader(data_file))
         except:
             # no file to load data from was found -> program data corrupted
             messagebox.showerror(title='Error', message='There was an issue loading the program data.\nPlease contact support.')
             window.destroy()
 
+    # force focus on the main window of the application 
+    window.focus_force()
     # no data in words_to_learn   
     if len(words_to_learn) <= 0:
         restart_words()
     # at least one entry in words_to_learn
     else:
         # get a reference to the name of the languages
-        languages = [language for language in words_to_learn[0].keys()]
+        current_languages = [language for language in words_to_learn[0].keys()]
         generate_card()
 
 
 
 # ------------------ SAVE PROGRESS ------------------ #
-# TODO: save progress
-# remove word from list of words that might come up, if right button is pressed
 def is_known_word():
     '''Removes current selected card from the list of words to know before rendering the next card.
     Called when the user clicks the right_button.'''
@@ -78,31 +122,38 @@ def is_known_word():
 
 
 
-# save filtered list of words in a words_to_learn.csv file
 def save_progress_on_close():
     '''Saves the current list of words to know to a save file and closes the app. Called when the user clicks to close the app'''
      # tap into the global variables for list of words to learn and languages
     global words_to_learn
-    global languages
-    # save data to a save file csv
-    with open(SAVE_FILE, 'w', newline='', encoding='utf-8-sig') as save_file:
-        save_data = csv.DictWriter(save_file, fieldnames=[languages[0], languages[1]])
-        save_data.writeheader()
-        save_data.writerows(words_to_learn)
+    global current_languages
+    try:
+        # save data to a save file csv
+        with open(saved_progress_file, 'w', newline='', encoding='utf-8-sig') as save_file:
+            save_data = csv.DictWriter(save_file, fieldnames=[current_languages[0], current_languages[1]])
+            save_data.writeheader()
+            save_data.writerows(words_to_learn)
+    except:
+        # no language selected
+        pass
+
     # close window
     window.destroy()
 
 
 
-import os
 def restart_words():
     '''Notifies the user that there's no new words to learn in the saved progress, and asks if they want to restart.'''
     # show messabox asking if user wants to restart
     restart = messagebox.askyesno(title='You nailed it!', message='There are no new words to learn. Restart list to practice more?')
     # yes
     if restart:
-        # delete save file with user's progress
-        os.remove(SAVE_FILE)
+        try:
+            # delete save file with user's progress
+            os.remove(saved_progress_file)
+        except:
+            # no save file -> to cover when user checked all words in the first go and don't generate a save before trigerring restart
+            pass
         # call start_app to load the app again and read from the base file, since save no longer exists
         start_app()
     # no
@@ -113,8 +164,8 @@ def restart_words():
         save_progress_on_close()
 
 
+
 # -------------- GENERATE / FLIP CARDS -------------- #
-import random
 def generate_card():
     '''It selects a random dictionary in the list of words_to_know and generates the front of a flash card.
     Calls flip_card to show the back of the card after the amount of time defined in the FLIP_DELAY constant.'''
@@ -127,7 +178,7 @@ def generate_card():
     # tap into the global variables for list of words to learn, selected card and languages
     global words_to_learn
     global selected_card
-    global languages
+    global current_languages
     # check if there are no more words to learn
     if len(words_to_learn) <= 0:
         restart_words()
@@ -136,9 +187,9 @@ def generate_card():
         # get a random dict from the list and assign it to the global selected_card
         selected_card = random.choice(words_to_learn)
         # change card_language text with the first language in the languages list
-        card.itemconfig(card_language, text=languages[0], fill='#000')
+        card.itemconfig(card_language, text=current_languages[0], fill='#000')
         # change card_word text to the value in selected_card at the key that corresponds to the first language in the languages list
-        card.itemconfig(card_word, text=selected_card[languages[0]], fill='#000')
+        card.itemconfig(card_word, text=selected_card[current_languages[0]], fill='#000')
         # change card background image
         card.itemconfig(card_bg, image=card_front_bg)
         # disable buttons so they cannot be clicked while the front of the card is being displayed
@@ -155,11 +206,11 @@ def flip_card():
     Called by generate_card after some amount of time defined in the FLIP_DELAY constant.'''
     # tap into the global variables for selected card and languages
     global selected_card
-    global languages
+    global current_languages
     # change card_language text with the second language in the languages list
-    card.itemconfig(card_language, text=languages[1], fill='#FFF')
+    card.itemconfig(card_language, text=current_languages[1], fill='#FFF')
     # change card_word text to the value in selected_card at the key that corresponds to the second language in the languages list
-    card.itemconfig(card_word, text=selected_card[languages[1]], fill='#FFF')
+    card.itemconfig(card_word, text=selected_card[current_languages[1]], fill='#FFF')
     # change card backgroud image
     card.itemconfig(card_bg, image=card_back_bg)
     # enable buttons so they can be clicked while back of the card is being displayed
@@ -169,8 +220,7 @@ def flip_card():
 
 
 # -------------------- UI SETUP --------------------- #
-# TODO: create UI using Tkinter
-from tkinter import *
+#create UI using Tkinter
 # create window
 window = Tk()
 window.title('Flash Card App')
@@ -187,17 +237,17 @@ card.grid(row=0, column=0, columnspan=2)
 
 # create the buttons for right and wrong answer
 right_img = PhotoImage(file=RIGHT_BUTTON_IMG)
-right_button = Button(image=right_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command=is_known_word)
+right_button = Button(image=right_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command=is_known_word, state='disabled')
 right_button.grid(row=1, column=0)
 
 wrong_img = PhotoImage(file=WRONG_BUTTON_IMG)
-wrong_button = Button(image=wrong_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command=generate_card)
+wrong_button = Button(image=wrong_img, highlightthickness=0, relief=FLAT, bg=BG_COLOR, command=generate_card, state='disabled')
 wrong_button.grid(row=1, column=1)
 
 # save progress on close
 window.protocol("WM_DELETE_WINDOW", save_progress_on_close)
 
-# start app -> read data from file
-start_app()
+# ----- ON PROGRAM START -> show pop up to select language
+choose_language_pop_up(window)
 
 window.mainloop()
