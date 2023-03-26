e0ece76d9e6fabebcd4a071c4f8ca6d3aaad5f03
Thais Campos <thaismca.bio@gmail.com>
2023-03-26 15:20:55 -0700

    Sunrise and sunset local hours and minutes to tuple - created a function that converts an integer that represents the UTC hour to the local hour, considering the LOCAL_UTC_OFFSET, to convert the hours for both sunrise and sunset coming from the API. - created a function that receives a time string expressed following ISO 8601 and returns a tuple containing the local hour and minutes.

--- Udemy - 100 days of Python/Intermediate sections/day-33_iss-overhead-notifier/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-33_iss-overhead-notifier/main.py
@@ -18,6 +18,28 @@ parameters = {
     "formatted": 0
 }
 
+# TODO: convert both sunrise and sunset to local time, considering the LOCAL_UTC_OFFSET
+def utc_to_local(utc_hour):
+    '''Converts an integer that represents the UTC hour to the local hour, considering the LOCAL_UTC_OFFSET.'''
+    utc_hour += LOCAL_UTC_OFFSET
+    if LOCAL_UTC_OFFSET > 0:
+        if utc_hour > 23:
+            utc_hour -= 24
+    elif LOCAL_UTC_OFFSET < 0:
+        if utc_hour < 0:
+            utc_hour += 24
+    return utc_hour
+
+# TODO: generate a tuple for both sunrise and sunset times, containing the local hour and minutes for each one of them
+def get_local_hour_minutes(time):
+    '''Receives a time string expressed following ISO 8601 and returns a tuple containing the local hour and minutes.'''
+    utc_hour = int(time.split("T")[1].split(":")[0])
+    local_hour = utc_to_local(utc_hour)
+    minutes = int(time.split("T")[1].split(":")[1])
+
+    return(local_hour, minutes)
+
+
 # make a get request to Sunset and sunrise times API
 response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
 # raise exception if no success
@@ -25,15 +47,12 @@ response.raise_for_status()
 # get a reference to the response data in JSON format
 data = response.json()
 # get references to both sunrise and sunset data from the JSON file
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = get_local_hour_minutes(data["results"]["sunrise"])
+sunset = get_local_hour_minutes(data["results"]["sunset"])
 
 print(sunrise)
 print(sunset)
 
-
-# TODO: convert both sunrise and sunset to local time, considering the LOCAL_UTC_OFFSET
-# TODO: generate a tuple for both sunrise and sunset times, containing the local hour and minutes for each one of them
 # TODO: generate a tuple with the current hour and minutes
 # TODO: make a request to the ISS current location API and get references to the current ISS coordinates
 # TODO: check if the current ISS is close to my location considering MY_LAT and MY_LONG, and if it's currently dark
