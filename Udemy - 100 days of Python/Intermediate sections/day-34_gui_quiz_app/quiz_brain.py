fc1843b692d26be3f527552a8746eb05cce3914d
Thais Campos <thaismca.bio@gmail.com>
2023-03-27 16:26:40 -0700

    Unescaping HTML Entities - added code to unescape HTML entities in the question texts

--- Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/quiz_brain.py
+++ Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
     """Models the Quiz Brain"""
     def __init__(self, question_list):
@@ -10,11 +12,13 @@ class QuizBrain:
         # get reference to the question in the question list which index corresponds to question_number
         question = self.question_list[self.question_number]
         # increment question_number
-        # this allows program to retrieve the questio from the next index the next time the function is called during program execution
+        # this allows program to retrieve the question from the next index the next time the function is called during program execution
         # it also corrects the number to be displayed for the current question
         self.question_number += 1
+        # get the text from the current question and unescape it
+        question_text = html.unescape(question.text)
         # print question in the input request
-        user_answer = input(f"Q.{self.question_number}: {question.text} (True / False?): ").lower()
+        user_answer = input(f"Q.{self.question_number}: {question_text} (True / False?): ").lower()
         self.is_correct_answer(user_answer, question.answer)
 
     def still_has_questions(self):
