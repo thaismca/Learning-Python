aae5eb57f53612460524b26addd48d06ac7673b1
Thais Campos <thaismca.bio@gmail.com>
2023-03-27 16:14:22 -0700

    Getting questions from API - replaced hardcoded questions data for an API request to the Open Trivia DB, getting 10 random questions from any category each time a new quiz starts

--- Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/main.py
@@ -1,14 +1,14 @@
 # This code implements a Quiz Game as part of the day 17 of the python course
 # goal: get practice with oop and python syntax when defining custom classes
 
-from data import question_data
+from data import questions_data
 from question_model import Question
 from quiz_brain import QuizBrain
 
 # create question bank
 question_bank = []
 # for each question in the question_data list
-for question in question_data:
+for question in questions_data:
     # get references to the text and answer attributes for that question
     q_text = question['question']
     q_answer = question['correct_answer']
