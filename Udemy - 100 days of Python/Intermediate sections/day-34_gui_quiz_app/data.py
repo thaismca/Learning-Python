6ad29920f17af03c3f29093d4fc34867a15de0d3
Thais Campos <thaismca.bio@gmail.com>
2023-04-20 10:41:24 -0700

    added ability to set number of questions and difficulty level before starting the quiz

--- Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/data.py
+++ Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/data.py
@@ -1,11 +1,22 @@
 import requests
+from question_model import Question
 
-parameters = {
-    'amount': 10,
-    'type': 'boolean'
-}
 
-response = requests.get("https://opentdb.com/api.php", params=parameters)
-response.raise_for_status()
+class QuizData:
+    def __init__(self, params):
+        self.params = params
+        self.quiz_data = requests.get(url="https://opentdb.com/api.php", params=self.params)
+        self.question_data = self.quiz_data.json()["results"]
+        
+        self.generate_questions()
 
-questions_data = response.json()["results"]
+
+    def generate_questions(self):
+        self.question_bank = []
+        for question in self.question_data:
+            question_text = question["question"]
+            question_category = question["category"]
+            question_answer = question["correct_answer"]
+            new_question = Question(question_text, question_category, question_answer)
+            self.question_bank.append(new_question)
+
--- Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-34_gui_quiz_app/main.py
@@ -1,25 +1,8 @@
 # This code implements a Quiz Game as part of the day 17 of the python course
 # goal: get practice with oop and python syntax when defining custom classes
 
-from data import questions_data
-from question_model import Question
-from quiz_brain import QuizBrain
 from ui import QuizzInterface
 
-# create question bank
-question_bank = []
-# for each question in the question_data list
-for question in questions_data:
-    # get references to the text and answer attributes for that question
-    q_text = question['question']
-    q_category = question['category']
-    q_answer = question['correct_answer']
-    # create an object from the class Question using the text and answer attributes
-    new_question = Question(q_text, q_category, q_answer)
-    # append this object to the question_bank list
-    question_bank.append(new_question)
 
-# create an object from the QuizBrain class using the question_bank list
-quiz = QuizBrain(question_bank)
 # create an object from the QuizInterface class passing the quiz object from the class QuizBrain 
-quiz_ui = QuizzInterface(quiz)
+quiz_ui = QuizzInterface()
