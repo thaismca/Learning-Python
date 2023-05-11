b3e9fd48daa998809004c33d602b550126a34506
Thais Campos <thaismca.bio@gmail.com>
2023-05-11 15:21:46 -0700

    Retrieve weather data for a given location (Vancouver, BC) from the Open Weather API

--- Udemy - 100 days of Python/Intermediate sections/day-35_rain-alert-sms/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-35_rain-alert-sms/main.py
@@ -1,5 +1,15 @@
+import requests
+
 # TODOS:
 # Retrieve weather data for a given location from the Open Weather API
+api_key = "a099ba2c15bd323e64db8198dd5febdb"
+my_lat = 49.282730
+my_long = -123.120735
+
+request_data = requests.get(url=f"https://api.openweathermap.org/data/2.8/onecall?lat={my_lat}&lon={my_long}&appid={api_key}")
+print(request_data.json())
+
+
 # Check if it will rain in that location in the next 12 hours
 # Send SMS via the Twilio API
 # Use PythonAnywhere to automate the Python Script
