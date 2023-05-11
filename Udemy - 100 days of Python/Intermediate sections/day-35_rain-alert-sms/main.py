b80d2fcd5143c89a7825fe0245be598269bbbf25
Thais Campos <thaismca.bio@gmail.com>
2023-05-11 16:35:58 -0700

    Check if it will rain or snow in that location in the next 12 hours

--- Udemy - 100 days of Python/Intermediate sections/day-35_rain-alert-sms/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-35_rain-alert-sms/main.py
@@ -6,11 +6,17 @@ api_key = "a099ba2c15bd323e64db8198dd5febdb"
 my_lat = 49.282730
 my_long = -123.120735
 
-request_data = requests.get(url=f"https://api.openweathermap.org/data/2.8/onecall?lat={my_lat}&lon={my_long}&appid={api_key}")
-print(request_data.json())
-
+request_data = requests.get(url=f"https://api.openweathermap.org/data/2.8/onecall?lat={my_lat}&lon={my_long}&exclude=current,minutely,daily&appid={api_key}")
+request_data.raise_for_status()
 
 # Check if it will rain in that location in the next 12 hours
+# rain weather ids are all <600 and snow are the 6xx -> this implementation will include snow in the alert
+twelve_hours_forecast = request_data.json()['hourly'][:12]
+for hour in twelve_hours_forecast:
+    if (hour['weather'][0]['id'] < 700):
+        print('Bring an umbrella')
+        break
+
 # Send SMS via the Twilio API
 # Use PythonAnywhere to automate the Python Script
 # Hide API Keys -> Environment Variables
