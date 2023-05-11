c5e1db1c851c77f48856dd264a3c8c68f3ebfd05
Thais Campos <thaismca.bio@gmail.com>
2023-05-11 15:20:23 -0700

    project setup and breakdown into todos

---
+++ Udemy - 100 days of Python/Intermediate sections/day-35_rain-alert-sms/main.py
@@ -0,0 +1,6 @@
+# TODOS:
+# Retrieve weather data for a given location from the Open Weather API
+# Check if it will rain in that location in the next 12 hours
+# Send SMS via the Twilio API
+# Use PythonAnywhere to automate the Python Script
+# Hide API Keys -> Environment Variables
