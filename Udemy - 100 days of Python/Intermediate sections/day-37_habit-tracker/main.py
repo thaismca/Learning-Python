6d3ae30ca89a14f1026ae0c2a38a14cd537d8dda
Thais Campos <thaismca.bio@gmail.com>
2023-05-17 17:38:22 -0700

    added code that creates a user in Pixela

--- Udemy - 100 days of Python/Intermediate sections/day-37_habit-tracker/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-37_habit-tracker/main.py
@@ -1,8 +1,27 @@
+import os
+import requests
+
+# retrieving hiden sensitive information -> Environment Variables
+from dotenv import load_dotenv
+load_dotenv()
+
 # COURSE SCOPE
 
 # STEP 1: create user in Pixela
 ## endpoint: 'https://pixe.la/v1/users'
 ## username must be unique -> if you try to create a user passing a username that already exists, you will get a 409 response
+create_user_endpoint = 'https://pixe.la/v1/users'
+user_params = {
+    "token": os.environ["USER_TOKEN"],
+    "username": os.environ["USER_NAME"],
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+res = requests.post(url=create_user_endpoint, json=user_params)
+print(res.text)
+res.raise_for_status()
+
 
 # STEP 2: create a new graph for the user
 ## endpoint: 'https://pixe.la/v1/users/<username>/graphs'
