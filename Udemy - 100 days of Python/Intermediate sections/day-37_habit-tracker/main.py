31f7a3ce4727a58f5998913d9c7a7ea31c89dc0f
Thais Campos <thaismca.bio@gmail.com>
2023-05-17 17:39:17 -0700

    added code that creates a new graph for an user in Pixela

--- Udemy - 100 days of Python/Intermediate sections/day-37_habit-tracker/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-37_habit-tracker/main.py
@@ -28,6 +28,23 @@ res.raise_for_status()
 ## must provide id, name, unit, type, and color parameters
 ## token must be provided in the request headers
 ## more details in the docs: https://docs.pixe.la/entry/post-graph
+create_graph_endpoint = f'https://pixe.la/v1/users/{os.environ["USER_NAME"]}/graphs'
+graph_params = {
+    "id": "graph1",
+    "name": "Sample Graph",
+    "unit": "times",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": os.environ["USER_TOKEN"]
+}
+
+res = requests.post(url=create_graph_endpoint, json=graph_params, headers=headers)
+print(res.headers)
+print(res.text)
+res.raise_for_status()
 
 # STEP 3: add a pixel to the habit tracker
 ## endpoint: 'https://pixe.la/v1/users/<username>/graphs/<graphID>'
