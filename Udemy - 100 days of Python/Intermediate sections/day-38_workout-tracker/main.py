13208eaee7ea13de1c0953c0abc54df5941bb74b
Thais Campos <thaismca.bio@gmail.com>
2023-05-19 13:43:55 -0700

    Use the Sheety API to generate a new row of data in your Google Sheet for each of the exercises that comes back from the Nutritionix API

--- Udemy - 100 days of Python/Intermediate sections/day-38_workout-tracker/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-38_workout-tracker/main.py
@@ -1,10 +1,14 @@
 import os
 import requests
+import json
 import datetime as dt
 
 # retrieving hiden sensitive information -> Environment Variables
 from dotenv import load_dotenv
 load_dotenv()
+
+# get today's date
+today = dt.datetime.now()
 
 # Get Exercise Stats with Natural Language Queries
 nutritionix_exercise_endpoint= 'https://trackapi.nutritionix.com/v2/natural/exercise'
@@ -22,5 +26,28 @@ nutritionix_exercise_req_body = {
 }
 
 nutritionix_res = requests.post(url=nutritionix_exercise_endpoint, json=nutritionix_exercise_req_body, headers=nutritionix_exercise_req_headers)
-print(nutritionix_res.text)
-nutritionix_res.raise_for_status()
+try:
+    nutritionix_res_data = json.loads(nutritionix_res.text)
+
+    # Use the Sheety API to generate a new row of data in your Google Sheet for each of the exercises that comes back from the Nutritionix API
+    sheety_add_row_endpoint= 'https://api.sheety.co/ecd388c9c56d18a14d531a5e0532f1b9/workoutTracking/workouts'
+    sheety_add_row_req_headers = {
+        "Authorization": "Basic " + os.environ.get('SHEETY_AUTH'),
+    }
+
+    for exercise in nutritionix_res_data['exercises']:
+        sheety_add_row_req_body = {
+            "workout": {
+                "date": today.strftime("%d/%m/%Y"),
+                "time": today.strftime("%H:%M"),
+                "exercise": exercise['name'],
+                "duration": exercise['duration_min'],
+                "calories": exercise['nf_calories']
+            }
+        }
+
+        sheety_res = requests.post(url=sheety_add_row_endpoint, json=sheety_add_row_req_body, headers=sheety_add_row_req_headers)
+        sheety_res.raise_for_status()
+
+except ValueError:
+    print('An error occured. Please try again.')
