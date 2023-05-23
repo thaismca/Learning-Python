b3ae8555a72ebe732de4bc382e53c02f75cfacb9
Thais Campos <thaismca.bio@gmail.com>
2023-05-22 17:42:35 -0700

    retrieve IATA codes for each city in the spreadsheet

--- Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/main.py
@@ -4,10 +4,44 @@
 ## International Air Transport Association (IATA) codes for each city.
 ## Most of the cities in the sheet include multiple airports, you want the city code (not the airport code).
 
+import requests
+import os
+
+# retrieving hiden sensitive information -> Environment Variables
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# get data from spreadsheet
+sheety_req_headers = {
+    "Authorization": "Basic " + os.environ.get('SHEETY_AUTH'),
+}
+sheety_get_rows_endpoint= 'https://api.sheety.co/ecd388c9c56d18a14d531a5e0532f1b9/flightDeals/prices'
+                
+sheety_res = requests.get(url=sheety_get_rows_endpoint, headers=sheety_req_headers)
+sheety_res.raise_for_status()
+sheety_res_data = sheety_res.json()['prices']
+
+# get IATA codes for each city
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+for row in sheety_res_data:
+    if row["iataCode"] == "":
+        headers = {
+            "apikey": os.environ.get('TEQUILA_API_KEY')
+        }
+        params = {
+            "term": row["city"],
+            "location_types": "city"
+        }
+        location_res = requests.get(f'{TEQUILA_ENDPOINT}/locations/query', params=params,headers=headers)
+        location_res.raise_for_status()
+        location_res_data = location_res.json()["locations"]
+        print(location_res_data)
+
+
+
 ## Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the cities in the Google Sheet.
 
 ## If the price is lower than the lowest price listed in the Google Sheet then send an SMS to your own number with the Twilio API.
 ## The SMS should include the departure airport IATA code, destination airport IATA code, departure city, 
 ## destination city, flight price and flight dates.
-
-
