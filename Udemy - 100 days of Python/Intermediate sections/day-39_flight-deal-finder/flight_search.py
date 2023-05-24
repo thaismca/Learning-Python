57d6bd57b9da5700a59fda8cf0c1a56b0afd29e7
Thais Campos <thaismca.bio@gmail.com>
2023-05-23 18:02:41 -0700

    moving code that checks for flights to the FlightSearch class

--- Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/flight_search.py
+++ Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/flight_search.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime, timedelta
 
 # retrieving hiden sensitive information -> Environment Variables
 from dotenv import load_dotenv
@@ -9,18 +10,55 @@ TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
+    def __init__(self) -> None:
+        # request headers
+        self.headers = {
+            "apikey": os.environ.get('TEQUILA_API_KEY')
+        }
+
+        # flight search criteria
+        self.origin = 'YVR'
+        self.depart_date_from = (datetime.now() + timedelta(1)).date()
+        self.depart_date_to = (datetime.now() + timedelta(weeks=24)).date()
+        self.min_stay = 7
+        self.max_stay = 28
+        self.currency = 'CAD'
+
+
 
     def get_iata_code(self, city):
         '''This function returns the IATA code for a given city'''
-        headers = {
-            "apikey": os.environ.get('TEQUILA_API_KEY')
-        }
         params = {
             "term": city,
             "location_types": "city"
         }
-        location_res = requests.get(f'{TEQUILA_ENDPOINT}/locations/query', params=params,headers=headers)
+        location_res = requests.get(f'{TEQUILA_ENDPOINT}/locations/query', params=params,headers=self.headers)
         location_res.raise_for_status()
         location_res_data = location_res.json()["locations"]
         
         return location_res_data[0]["code"]
+    
+
+
+    def find_flights(self, city, max_price):
+        '''This function returns all the flights that match the search criteria.'''
+
+        ## In this project, we're looking only for direct flights, that leave anytime between tomorrow and in 6 months time.
+        ## We're also looking for round trips that return between 7 and 28 days in length.
+        ## The currency of the price we get back should be in CAD.
+        params = {
+            "fly_from": self.origin,
+            "fly_to": city,
+            "date_from": self.depart_date_from,
+            "date_to": self.depart_date_to,
+            "nights_in_dst_from": self.min_stay,
+            "nights_in_dst_to": self.max_stay,
+            "flight_type": "round",
+            "curr": self.currency,
+            "price_to": max_price
+        }
+        flights_res = requests.get(f'{TEQUILA_ENDPOINT}/search', params=params,headers=self.headers)
+        flights_res.raise_for_status()
+        flights_res_data = flights_res.json()["data"]
+
+        return flights_res_data
--- Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/main.py
+++ Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/main.py
@@ -2,56 +2,27 @@
 import os
 import requests
 
-## Use the Flight Search and Sheety API to populate your own copy of the Google Sheet with
-## International Air Transport Association (IATA) codes for each city.
-## Most of the cities in the sheet include multiple airports, you want the city code (not the airport code).
 
 # get data from spreadsheet
 from data_manager import DataManager
 data_manager = DataManager()
 sheety_res_data = data_manager.get_prices_sheet_data()
 
-## Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the cities in the Google Sheet.
-## In this project, we're looking only for direct flights, that leave anytime between tomorrow and in 6 months time.
-## We're also looking for round trips that return between 7 and 28 days in length.
-## The currency of the price we get back should be in CAD.
-from datetime import datetime, timedelta
-ORIGIN = 'YVR'
-DEPART_DATE_FROM = (datetime.now() + timedelta(1)).date()
-DEPART_DATE_TO = (datetime.now() + timedelta(weeks=24)).date()
-MIN_STAY = 7
-MAX_STAY = 28
-CURRENCY = 'CAD'
-
 # get IATA codes for each city and add them to the respective column in the spreadsheet
 from flight_search import FlightSearch
 flight_search = FlightSearch()
-for row in sheety_res_data:
-    if row["iataCode"] == "":
-        iataCode = flight_search.get_iata_code(row["city"])
-        data_manager.update_iata_code(row["id"], iataCode)
-    
-    ## Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the cities in the Google Sheet.
-    TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-    headers = {
-        "apikey": os.environ.get('TEQUILA_API_KEY')
-    }
-    params = {
-        "fly_from": ORIGIN,
-        "fly_to": row["iataCode"],
-        "date_from": DEPART_DATE_FROM,
-        "date_to": DEPART_DATE_TO,
-        "nights_in_dst_from": MIN_STAY,
-        "nights_in_dst_to": MAX_STAY,
-        "flight_type": "round",
-        "curr": CURRENCY,
-        "price_to": row["lowestPrice"]
-    }
+try:
+    for row in sheety_res_data:
+        ## Use the Flight Search and Sheety API to populate your own copy of the Google Sheet with IATA Codes for each city that doesn't have one
+        if row["iataCode"] == "":
+            iataCode = flight_search.get_iata_code(row["city"])
+            data_manager.update_iata_code(row["id"], iataCode)
+        
+        ## Use the Flight Search API to check for the cheapest flights from tomorrow to 6 months later for all the cities in the Google Sheet.
+        print(flight_search.find_flights(row["iataCode"], row["lowestPrice"]))
 
-    flights_res = requests.get(f'{TEQUILA_ENDPOINT}/search', params=params,headers=headers)
-    flights_res.raise_for_status()
-    flights_res_data = flights_res.json()
-    print(flights_res_data)
+except KeyError:
+    print('Spreadsheet data is corrupted. Cannot proceed with operation.')
 
 
 
