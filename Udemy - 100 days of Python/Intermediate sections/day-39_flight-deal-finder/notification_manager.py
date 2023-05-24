fc59dd786aceed72cc2d137f47b1e636dffe477d
Thais Campos <thaismca.bio@gmail.com>
2023-05-24 14:14:43 -0700

    adding code to tell Twilio client how to connect to the proxy server that free accounts use to access the external Internet, so it can run from PythonAnywhere

--- Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/notification_manager.py
+++ Udemy - 100 days of Python/Intermediate sections/day-39_flight-deal-finder/notification_manager.py
@@ -1,14 +1,22 @@
 import os
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 # retrieving hiden sensitive information -> Environment Variables
 from dotenv import load_dotenv
 load_dotenv()
 
+# Use PythonAnywhere to automate the Python Script
+try:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+except:
+    proxy_client = None
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.client = Client(os.environ.get('TWILIO_SID'), os.environ.get('TWILIO_AUTH_TOKEN'))
+        self.client = Client(os.environ.get('TWILIO_SID'), os.environ.get('TWILIO_AUTH_TOKEN'), http_client=proxy_client)
 
 
     def send_sms(self, message):
