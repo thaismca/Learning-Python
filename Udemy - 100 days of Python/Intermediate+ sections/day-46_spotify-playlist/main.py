0cfbe66105505f568f63d63b9cae4981b78acb5c
Thais Campos <thaismca.bio@gmail.com>
2023-05-26 14:06:55 -0700

    project setup and breakdown

---
+++ Udemy - 100 days of Python/Intermediate+ sections/day-46_spotify-playlist/main.py
@@ -0,0 +1,5 @@
+# Create an input prompt that asks what year the user would like to travel to in YYY-MM-DD format
+# Scrape the top 100 song titles on that date into a Python List using BeautifulSoup
+# Authenticate with Spotify using your unique Client ID/ Client Secret
+# Create a list of Spotify song URIs for the list of song names that were found from by scraping billboard 100
+# Create a new private playlist and each of the songs to the new playlist
