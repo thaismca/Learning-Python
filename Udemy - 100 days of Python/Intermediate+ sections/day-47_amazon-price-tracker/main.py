fcf72601fbd59b51159b819acad58db5b6c3f6ac
Thais Campos <thaismca.bio@gmail.com>
2023-07-14 16:32:22 -0700

    Email Alert When Price Below Preset Value - Set a threshold for the price of the item. - When the price is below the threshold, use the smtp module to send an email. - In the email, include the title of the product, the current price and a link to buy the product.

--- Udemy - 100 days of Python/Intermediate+ sections/day-47_amazon-price-tracker/main.py
+++ Udemy - 100 days of Python/Intermediate+ sections/day-47_amazon-price-tracker/main.py
@@ -12,15 +12,43 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import smtplib
+import os
+from dotenv import load_dotenv
 
+# get access to environment variables
+load_dotenv()
+
+#-- PRODUCT DETAILS -> replace with any desired Amazon product URL and price threshold -----------------------------#
 PRODUCT_URL = 'https://www.amazon.ca/Apple-AirPods-Charging-Latest-Model/dp/B07PXGQC1Q/ref=zg_bs_g_electronics_sccl_4/133-4231630-2799038?psc=1'
+PRICE_THRESHOLD = 190.00
+
+
+#-- Use BeautifulSoup to Scrape the Product Price -----------------------------#
+# request headers
 HEADERS = {
     'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 }
-
+# request the HTML page and make soup with what is returned
 res = requests.get(PRODUCT_URL, headers=HEADERS)
 soup = BeautifulSoup(res.text, 'lxml')
-
+# scrape the price as float
 price = soup.find(class_="a-offscreen").get_text().split("$")[1]
 price_float = float(price)
+
+
+#-- Email Alert When Price Below Preset Value -----------------------------#
+if price_float <= PRICE_THRESHOLD:
+    # get hold of product's name
+    product_name = soup.find(id="productTitle").get_text().strip()
+    # create message
+    message = f"{product_name} is now ${price}\n{PRODUCT_URL}"
+    
+    # send email to list of recipients
+    connection = smtplib.SMTP(os.environ.get('HOST'), os.environ.get('PORT'))
+    connection.starttls()
+    connection.login(os.environ.get('SENDER_EMAIL'), os.environ.get('PASSWORD'))
+    connection.sendmail(from_addr=os.environ.get('SENDER_EMAIL'),
+                        to_addrs= os.environ.get('RECIPIENT'),
+                        msg=f'Subject:Amazon Price Alert: {product_name}\n\n{message}')
