e4fe49c5d2128123ece9ca924932bdf6128eda84
Thais Campos <thaismca.bio@gmail.com>
2023-07-24 18:28:37 -0700

    practice 2: generate and print a nested dictionary containing the upcoming events from python.org

--- Udemy - 100 days of Python/Intermediate+ sections/day-48_intro-selenium-webdriver/practices.py
+++ Udemy - 100 days of Python/Intermediate+ sections/day-48_intro-selenium-webdriver/practices.py
@@ -10,14 +10,33 @@ options = ChromeOptions()
 options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(service=service, options=options)
 
-# open the product page using the webdriver
-driver.get('https://www.amazon.ca/Instant-Pot-Duo-Multi-Use-Programmable/dp/B00FLYWNYQ/ref=sr_1_3?crid=25ED8OV1FQ8DG&keywords=instant+pot&qid=1690073702&sprefix=instant+po%2Caps%2C154&sr=8-3')
-# get the price of the item using the class of the element
-price_by_class = driver.find_element(by=By.CLASS_NAME, value='a-offscreen')
-print(price_by_class.get_attribute('innerHTML'))
-# get the title of the first review of the item using XPath
-review_by_xpath = driver.find_element(by=By.XPATH, value='//*[@id="customer_review-R3RJ8LHFT5TTDG"]/div[2]/a/span[2]')
-print(review_by_xpath.text) 
+# # PRACTICE 1: automated price check on Amazon
+# # open the product page using the webdriver
+# driver.get('https://www.amazon.ca/Instant-Pot-Duo-Multi-Use-Programmable/dp/B00FLYWNYQ/ref=sr_1_3?crid=25ED8OV1FQ8DG&keywords=instant+pot&qid=1690073702&sprefix=instant+po%2Caps%2C154&sr=8-3')
+# # get the price of the item using the class of the element
+# price_by_class = driver.find_element(by=By.CLASS_NAME, value='a-offscreen')
+# print(price_by_class.get_attribute('innerHTML'))
+# # get the title of the first review of the item using XPath
+# review_by_xpath = driver.find_element(by=By.XPATH, value='//*[@id="customer_review-R3RJ8LHFT5TTDG"]/div[2]/a/span[2]')
+# print(review_by_xpath.text) 
+# # quit the browser
+# # close -> closes a tab, quit -> closes all tabs and quits the browser entirely
+# driver.quit()
+
+
+
+# PRACTICE 2: upcoming events from python.org
+# open the python.org page using the webdriver
+driver.get('https://www.python.org/')
+# get the list of events using css selector
+events = driver.find_elements(by=By.CSS_SELECTOR, value='.event-widget li')
+# loop through the list of events to create nested dictionary
+events_dict = {}
+for index, event in enumerate(events):
+    time = event.find_element(by=By.TAG_NAME, value='time').get_attribute('datetime').split('T')[0]
+    name = event.find_element(by=By.TAG_NAME, value='a').text
+    events_dict[index] = {'time': time, 'name': name}
+print(events_dict)
 # quit the browser
 # close -> closes a tab, quit -> closes all tabs and quits the browser entirely
 driver.quit()
